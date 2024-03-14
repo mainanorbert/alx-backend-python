23f7afdbde7722a979369589524323af369c09e5
Norberts <mainanorbert@gmail.comm>
2024-03-14 15:01:11 +0300

    multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,7 +5,10 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """function returning a function"""
+    """
+    function taking float argument and returns a 
+    function that multiplies two floats
+    """
     def inner(n: float) -> float:
         """function used for mult."""
         return n * multiplier;
