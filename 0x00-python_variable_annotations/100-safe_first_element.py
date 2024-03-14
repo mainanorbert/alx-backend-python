c82014e0fb36242a5195b72127483fdb10b17c4d
Norberts <mainanorbert@gmail.comm>
2024-03-14 15:10:44 +0300

    duck typing

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""module to Duck typing - first element of a sequence"""
+
+from typing import Optional, Any
+
+
+def safe_first_element(lst: list) -> Optional[Any]:
+    """Returns the first element of a list, or None if the list is empty."""
+    if lst:
+        return lst[0]
+    else:
+        return None
