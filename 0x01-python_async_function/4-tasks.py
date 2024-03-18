ee9badef6f98a08e25cbabf4a23b50b57d9e72aa
Norberts <mainanorbert@gmail.comm>
2024-03-18 14:37:06 +0300

    return array for asyn func

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,6 +8,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """function returns list of delays"""
     delay = await asyncio.gather(*[task_wait_random(max_delay)
                                    for _ in range(n)])
     return delay
