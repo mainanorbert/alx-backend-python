ec3d9ffa8351fa36aea61895b9f849cd207dfd53
Norberts <mainanorbert@gmail.comm>
2024-03-19 13:48:38 +0300

    rand gen

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,8 +5,8 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> [float, None, None]:
     """a coroutine generating int btn (1, 10)"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0.0, 10.0)
+        yield random.randint(0, 10)
