4d0c82cc18ef2c229fd2d0bf99d0b569dc0e7e78
Norberts <mainanorbert@gmail.comm>
2024-03-19 13:41:58 +0300

    rand gen

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""module the define async func"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """a coroutine generating int btn (1, 10)"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0.0, 10.0)
