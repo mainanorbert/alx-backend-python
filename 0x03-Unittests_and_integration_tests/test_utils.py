6f970bb099f2c49ce067efcc939416843f8abb55
Norberts <mainanorbert@gmail.comm>
2024-04-08 16:39:40 +0300

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -46,5 +46,26 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(result, test_payload)
 
 
+class TestMemoize(unittest.TestCase):
+    """Parameterize and patch"""
+    def test_memoize(self):
+        """function     with memoiz class"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoiz
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            instance = TestClass()
+            result1 = instance.a_property()
+            result2 = instance.a_propert()
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
+
+
 if __name__ == "__main__":
     unittest.main()
